22c13a5c355f0f28842c81595faec0484603a6b9
Zullybur <mattcasiro@gmail.com>
2016-04-09 11:50:03 -0700

    lab01: completed step 1 and 2

---
+++ lab01/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "hello.h"
+void helloFunc()
+{
+     printf("Hello, world!\n");
+}
+void goodbyeFunc()
+{
+     printf("Goodbye, world!\n");
+}
---
+++ lab01/hello.h
@@ -0,0 +1,2 @@
+void helloFunc();  // print the string "Hello, world"
+void goodbyeFunc();  // print the string "Goodbye, world"
